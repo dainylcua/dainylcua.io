4c007ec1f65b199e0ff28232ccf76997cbaea8f0
Dainyl Cua <dainylcua@gmail.com>
2022-10-28 00:45:55 -0500

    Makes sections screen-height, wip:section responsive design

--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -12,11 +12,10 @@ export default function AboutMe() {
         <Header2>
           About Me
         </Header2>
-        
       </div>
       <div className="flex items-center">
         <div className="flex flex-col mr-16">
-          <p className="py-4">Hi! I am Dainyl Cua, and I create full-stack applications with a purpose and also dabble in writing technical content aimed at developers. My end goal is to get into a position where I can help underrepresented communities get into tech. I hope I can help you learn a thing or two!</p>
+          <p className="py-4">Hi! I am Dainyl Cua, and I create full-stack applications with a purpose and also dabble in <ExtLink href="https://dc-blog-omega.vercel.app/blogposts">writing technical content aimed at developers</ExtLink>. My end goal is to get into a position where I can help underrepresented communities get into tech. I hope I can help you learn a thing or two!</p>
           <p className="py-4">I graduated from Texas A&M University in 2020 with a Bachelors of Science in Chemical Engineering. I worked at an environmental consulting agency for 9 months, lost my job due to COVID, and started my journey into tech. After learning the fundamentals of web devlopment, I studied Software Engineering at <ExtLink href="https://generalassemb.ly/">General Assembly&apos;s</ExtLink> coding bootcamp, then developed a prototype Jamstack EdTech website under <ExtLink href="https://danieljs.io/">my bootcamp instructor</ExtLink>, and worked as a <ExtLink href="https://www.twilio.com/blog/introducing-developer-voices-intern-dainyl-cua">Software Engineer for Technical Content</ExtLink> at Twilio, the industry leader for cloud communications.</p>
           <p className="py-4">Out of work, you&apos;ll find me playing Dungeons & Dragons, gaming, cooking and baking, or messing with my pets! That is, if I haven&apos;t found something new to try out.</p>
         </div>
